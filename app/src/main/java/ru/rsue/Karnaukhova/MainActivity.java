0914deb69038e4e478466d07c83ee5d0c47dbbce
LightlyAfternoon <asukca2@gmail.com>
2024-04-07 03:10:22 +0300

    next is lists

--- app/src/main/java/ru/rsue/Karnaukhova/MainActivity.java
+++ app/src/main/java/ru/rsue/Karnaukhova/MainActivity.java
@@ -1,38 +1,18 @@
 package ru.rsue.Karnaukhova;
 
-import android.view.Gravity;
 import android.view.MenuItem;
-import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
 
 import com.google.android.material.navigation.NavigationView;
 import org.jetbrains.annotations.NotNull;
-import ru.rsue.Karnaukhova.database.ItemBaseHelper;
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout mDrawerLayout;
